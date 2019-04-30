d11d32c700d8cd9826817d42d4c119da6205b190
MelodyShih <yoyoshih13@gmail.com>
2019-04-30 18:56:09 -0400

    wrong input for statefixts1d

--- solveFOM.m
+++ solveFOM.m
@@ -3,9 +3,12 @@ function [Qh, Qv, time] = solveFOM(hinit, vinit, time, tstep, winit)
 
     Qh = zeros(Np*K, winit);
     Qv = zeros(Np*K, winit);
+    
+    h = hinit;
+    v = vinit;
     for i = 1:winit
         time_end = time + tstep;
-        [h, v] = StateFixTS1D(hinit, vinit, time, time_end, B, limiter);
+        [h, v] = StateFixTS1D(h, v, time, time_end, B, limiter);
         time = time_end;
         Qh(:,i) = reshape(h, [Np*K, 1]);
         Qv(:,i) = reshape(v, [Np*K, 1]);
