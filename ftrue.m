0f07787456ecf4c8f8f94960fded2645ee131c8c
MelodyShih <yoyoshih13@gmail.com>
2019-04-30 17:14:12 -0400

    remove the duplicated statefixts1d code

--- ftrue.m
+++ ftrue.m
@@ -5,7 +5,6 @@ function [fQ] = ftrue(Q,time,time_end)
     v = reshape(Q(Np*K+1:end), [Np, K]); % evaluate at all points first
     
     [h, v] = StateFixTS1D(h, v, time, time_end, B, limiter);
-    [h, v] = StateFixTS1D(h, v, time, time_end, B, limiter);
     fQ = [reshape(h, [Np*K, 1]);reshape(v,[Np*K, 1])]; 
 end
 
