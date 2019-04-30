b91c6ebb0222baa86f9266c52d92399d97986b0e
Karina <koval@cims.nyu.edu>
2019-04-29 22:34:24 -0400

    added adeim code for adaptation of U and p

---
+++ adeim.m
@@ -0,0 +1,17 @@
+function [Unew,Pnew] = adeim(U,P,S,Fp,Fs,r)
+    C = U(P,:)\Fp;
+    R = U(S,:)*C-Fs;
+    [~,Sv,Sr] = svd(R,0);
+    Sv = diag(Sv);
+    Ctpinv = pinv(C');
+    r = min([r,length(Sv)]);
+    for i = 1:r
+        alpha = -R*Sr(:,i);
+        beta = Ctpinv*Sr(:,i);
+        U(S,:) = U(S,:)+alpha*beta';
+    end
+    [Q,R] = qr(U); #Orthogonalization of U
+    Unew = Q; 
+    Pnew = qdeim(Unew); 
+end
+
